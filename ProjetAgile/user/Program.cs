1a2631062dde8f147b848de261d176ff726f7ad7
mdttn <theresemyduyen@outlook.com>
2024-03-19 11:48:55 -0400

    Start menu

--- ProjetAgile/user/Program.cs
+++ ProjetAgile/user/Program.cs
@@ -13,79 +13,107 @@ namespace ProjetAgile.user
         static void Main(string[] args)
         {
             List<Player> playerList = new List<Player>();
-            Player player1 = new Player();
-            Player player2 = new Player();
-            Player player3 = new Player();
-            Player[] players = new Player[5];
+            Player player;
+            int i = 0;
             Game[] game1 = new Game[10];
             Game[] game2 = new Game[10];
             Game[] game3 = new Game[10];
             int option;
 
-            // head title
-            Console.WriteLine("BIENVENUE AU JEU ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("\"Who wants to be a millionaire?\"");
-            Console.ResetColor();
-            Console.WriteLine("!\n");
-
-        label1:
-            // start menu
-            Console.WriteLine("MENU");
-            Console.WriteLine("[1] Inscription");
-            Console.WriteLine("[2] Leaderboard");
-            Console.WriteLine("[3] Quitter");
-            Console.Write("Option: ");
-            try
-            {
-                option = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("\nOption invalide.\n");
-                goto label1;
-            }
-
             do
             {
-                Console.Write("Votre prénom: ");
-                player1.FirstName = Console.ReadLine();
-            }
-            while (player1.FirstName == "");
+                player = new Player();
+                option = 0;
 
-            do
-            {
-                Console.Write("Votre nom de famille: ");
-                player1.LastName = Console.ReadLine();
-            }
-            while (player1.LastName == "");
+                Console.Clear();
+                // head title
+                Console.WriteLine("BIENVENUE AU JEU ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\"Who wants to be a millionaire?\"\n");
+                Console.ResetColor();
 
-        label2:
-            Console.Write("Votre âge: ");
-            try
-            {
-                player1.Age = Convert.ToInt32(Console.ReadLine());
-                if (player1.Age < 18)
+                // start menu
+                Console.WriteLine("MENU");
+                Console.WriteLine("[1] Inscription");
+                Console.WriteLine("[2] Leaderboard");
+                Console.WriteLine("[3] Quitter");
+                Console.Write("Option: ");
+                try
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("\nVous êtes trop jeune pour participer.\n");
-                    Console.ResetColor();
-                    Console.Clear();
-
+                    option = Convert.ToInt32(Console.ReadLine());                    
                 }
-                else
+                catch (Exception)
                 {
-                    playerList.Add(player1);
-                    Console.WriteLine("\nJOUEUR #1: " + player1.GetState() + "\n");
-                    //
-                    Console.Clear();
+                    // message de default
                 }
+                Console.WriteLine();
+
+                switch (option)
+                {
+                    case 1:
+                        do
+                        {
+                            Console.Write("Votre prénom: ");
+                            player.FirstName = Console.ReadLine();
+                        }
+                        while (player.FirstName == "");
+
+                        do
+                        {
+                            Console.Write("Votre nom de famille: ");
+                            player.LastName = Console.ReadLine();
+                        }
+                        while (player.LastName == "");
+
+                    label1:
+                        Console.Write("Votre âge: ");
+                        try
+                        {
+                            player.Age = Convert.ToInt32(Console.ReadLine());
+                            if (player.Age < 18)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("\nVous êtes trop jeune pour participer.");
+                                Console.ResetColor();
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                i++;
+                                playerList.Add(player);
+                                Console.WriteLine($"\nJoueur {i}: {player.GetState()}");
+                                Console.ReadLine();
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nÂge invalide.\n");
+                            Console.ResetColor();
+                            goto label1;
+                        }
+                        break;
+                    case 2:
+                        foreach (Player p in playerList)
+                        {
+                            Console.WriteLine($"{p.GetState()}");
+                        }
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\nCliquez 2 fois pour quitter.");
+                        Console.ResetColor();
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Option invalide.");
+                        Console.ResetColor();
+                        Console.ReadLine();
+                        break;
+                }                
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                goto label2;
-            }
+            while (option != 3);            
 
             Console.ReadKey();
         }
